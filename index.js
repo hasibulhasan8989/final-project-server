beb1ef4339b894c227d757a93d57fdfe730ee233
Hasibul Hasan <hasanchaion847@gmail.com>
2025-07-23 22:07:22 +0600

    cart delete api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zsgh3ij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -42,11 +42,17 @@ async function run() {
     //get item from cart
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      console.log(email)
       const query={email:email}
       const result = await carts.find(query).toArray();
       res.send(result);
     });
+    //delete from cart
+    app.delete('/carts/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id : new ObjectId(id)}
+      const result=await carts.deleteOne(query)
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
