62a0005fe83c575b84a8bc3f9cb831e3a2e1bd23
Hasibul Hasan <hasanchaion847@gmail.com>
2025-07-25 01:57:08 +0600

    api sucure

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.user;
+  console.log(email);
+  const query = { email: email };
+
+  const user = await users.findOne(query);
+  console.log(user);
+  const isAdmin = user.role === "admin";
+  if (!isAdmin) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  next();
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zsgh3ij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,10 +57,56 @@ async function run() {
       .collection("menuCollection");
     const carts = client.db("bistroBossDB").collection("carts");
     const users = client.db("bistroBossDB").collection("users");
-   
+
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
+    });
+
+
+// middle ware
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.user;
+  console.log(email);
+  const query = { email: email };
+
+  const user = await users.findOne(query);
+  console.log(user);
+  const isAdmin = user.role === "admin";
+  if (!isAdmin) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  next();
+};
+
+
+
+    ///jwt token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1y",
+      });
+      res.send({ token });
+    });
+
+    //check admin
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const userEmail = req.params.email;
+
+      if (userEmail !== req.decoded.user) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: userEmail };
+      const user = await users.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
 
     //add item to cart
@@ -42,32 +118,58 @@ async function run() {
 
     //add user after registration
 
-    app.post('/users',async(req,res)=>{
-      
-      const user=req.body;
-      const query={email:user.email}
-      const isExist=await users.findOne(query)
-      if(isExist){
-        return res.send({message:'Already Exit',insertedId:null})
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const isExist = await users.findOne(query);
+      if (isExist) {
+        return res.send({ message: "Already Exit", insertedId: null });
       }
-      const result=await users.insertOne(user)
-      res.send(result)
-    })
+      const result = await users.insertOne(user);
+      res.send(result);
+    });
+
+    //get all user in admin Dashboard
+
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await users.find().toArray();
+      res.send(result);
+    });
+
+    //delete a user
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await users.deleteOne(query);
+      res.send(result);
+    });
 
     //get item from cart
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      const query={email:email}
+      const query = { email: email };
       const result = await carts.find(query).toArray();
       res.send(result);
     });
+
     //delete from cart
-    app.delete('/carts/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id : new ObjectId(id)}
-      const result=await carts.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await carts.deleteOne(query);
+      res.send(result);
+    });
+
+    //make admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await users.updateOne(query, updatedDoc);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
